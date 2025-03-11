7e47b36bff999dee6b7f4fffd91b36057800de25
danemadsen <dane_madsen@hotmail.com>
2025-03-11 14:16:46 +1000

    try remove n_parallel

--- examples/simple-tts/simple-tts.cpp
+++ examples/simple-tts/simple-tts.cpp
@@ -303,15 +303,11 @@ static std::vector<llama_token> prepare_guide_tokens(const llama_vocab * vocab, 
     return result;
 }
 
-static void batch_add(struct llama_batch & batch, llama_token id,llama_pos pos, const std::vector<llama_seq_id> & seq_ids, bool logits) {
+static void batch_add(struct llama_batch & batch, llama_token id, llama_pos pos, bool logits) {
     GGML_ASSERT(batch.seq_id[batch.n_tokens] && "llama_batch size exceeded");
 
     batch.token   [batch.n_tokens] = id;
     batch.pos     [batch.n_tokens] = pos;
-    batch.n_seq_id[batch.n_tokens] = seq_ids.size();
-    for (size_t i = 0; i < seq_ids.size(); ++i) {
-    batch.seq_id[batch.n_tokens][i] = seq_ids[i];
-    }
     batch.logits  [batch.n_tokens] = logits;
 
     batch.n_tokens++;
@@ -490,7 +486,7 @@ static void print_usage(int, char ** argv) {
 }
 
 int main(int argc, char ** argv) {
-    const int n_parallel = 1;
+    //const int n_parallel = 1;
     const int n_predict  = 4096;
 
     std::string prompt;
@@ -581,12 +577,9 @@ int main(int argc, char ** argv) {
         return 1;
     }
 
-    std::vector<llama_sampler *> smpl(n_parallel);
-    for (int i = 0; i < n_parallel; ++i) {
-        smpl[i] = llama_sampler_chain_init(llama_sampler_chain_default_params());
-        llama_sampler_chain_add(smpl[i], llama_sampler_init_greedy());
-        llama_sampler_chain_add(smpl[i], llama_sampler_init_dist(LLAMA_DEFAULT_SEED));
-    }
+    llama_sampler * smpl = llama_sampler_chain_init(llama_sampler_chain_default_params());
+    llama_sampler_chain_add(smpl, llama_sampler_init_greedy());
+    llama_sampler_chain_add(smpl, llama_sampler_init_dist(LLAMA_DEFAULT_SEED));
 
     outetts_version tts_version = get_tts_version(model);
 
@@ -603,6 +596,8 @@ int main(int argc, char ** argv) {
 
     std::string prompt_clean = process_text(prompt, tts_version);
 
+    printf("prompt_clean: %s\n", prompt_clean.c_str());
+
     std::vector<llama_token> guide_tokens = prepare_guide_tokens(vocab, prompt_clean, tts_version);
 
     prompt_add(prompt_inp, vocab, prompt_clean, false, true);
@@ -613,16 +608,11 @@ int main(int argc, char ** argv) {
 
     // create a llama_batch
     // we use this object to submit token data for decoding
-    llama_batch batch = llama_batch_init(std::max(prompt_inp.size(), (size_t) n_parallel), 0, n_parallel);
-
-    std::vector<llama_seq_id> seq_ids(n_parallel, 0);
-    for (int32_t i = 0; i < n_parallel; ++i) {
-        seq_ids[i] = i;
-    }
+    llama_batch batch = llama_batch_init(prompt_inp.size(), 0, 1);
 
     // evaluate the initial prompt
     for (size_t i = 0; i < prompt_inp.size(); ++i) {
-        batch_add(batch, prompt_inp[i], i, seq_ids, false);
+        batch_add(batch, prompt_inp[i], i, false);
     }
 
     // llama_decode will output logits only for the last token of the prompt
@@ -639,7 +629,7 @@ int main(int argc, char ** argv) {
 
     // remember the batch index of the last token for each parallel sequence
     // we need this to determine which logits to sample from
-    std::vector<int32_t> i_batch(n_parallel, batch.n_tokens - 1);
+    int32_t i_batch = batch.n_tokens - 1;
 
     int n_past   = batch.n_tokens;
     int n_decode = 0;
@@ -652,38 +642,38 @@ int main(int argc, char ** argv) {
         batch.n_tokens = 0;
 
         // sample the next token for each parallel sequence / stream
-        for (int32_t i = 0; i < n_parallel; ++i) {
-            if (i_batch[i] < 0) {
-                // the stream has already finished
-                continue;
-            }
-
-            llama_token new_token_id = llama_sampler_sample(smpl[i], ctx, i_batch[i]);
-
-            //guide tokens help prevent hallucinations by forcing the TTS to use the correct word
-            if (!guide_tokens.empty() && next_token_uses_guide_token && !llama_vocab_is_control(vocab, new_token_id) && !llama_vocab_is_eog(vocab, new_token_id)) {
-                llama_token guide_token = guide_tokens[0];
-                guide_tokens.erase(guide_tokens.begin());
-                new_token_id = guide_token; //ensure correct word fragment is used
-            }
-
-            //this is the token id that always precedes a new word
-            next_token_uses_guide_token = (new_token_id == 198);
-
-            llama_sampler_accept(smpl[i], new_token_id);
-
-            codes.push_back(new_token_id);
-
-            if (llama_vocab_is_eog(vocab, new_token_id) || n_decode == n_predict) {
-                // Mark the stream as finished
-                i_batch[i] = -1;
-                continue;
-            }
-
-            i_batch[i] = batch.n_tokens;
-
-            batch_add(batch, new_token_id, n_past, { i }, true);
+        if (i_batch < 0) {
+            // the stream has already finished
+            printf("stream finished: i_batch < 0\n");
+            break;
         }
+
+        llama_token new_token_id = llama_sampler_sample(smpl, ctx, i_batch);
+
+        //guide tokens help prevent hallucinations by forcing the TTS to use the correct word
+        if (!guide_tokens.empty() && next_token_uses_guide_token && !llama_vocab_is_control(vocab, new_token_id) && !llama_vocab_is_eog(vocab, new_token_id)) {
+            llama_token guide_token = guide_tokens[0];
+            guide_tokens.erase(guide_tokens.begin());
+            new_token_id = guide_token; //ensure correct word fragment is used
+        }
+
+        //this is the token id that always precedes a new word
+        next_token_uses_guide_token = (new_token_id == 198);
+
+        llama_sampler_accept(smpl, new_token_id);
+
+        codes.push_back(new_token_id);
+
+        if (llama_vocab_is_eog(vocab, new_token_id) || n_decode == n_predict) {
+            // Mark the stream as finished
+            i_batch = -1;
+            printf("stream finished: EOG\n");
+            break;
+        }
+
+        i_batch = batch.n_tokens;
+
+        batch_add(batch, new_token_id, n_past, true);
 
         // all streams are finished
         if (batch.n_tokens == 0) {
@@ -702,6 +692,8 @@ int main(int argc, char ** argv) {
 
     llama_batch_free(batch);
 
+    printf("codes.size(): %zu\n", codes.size());
+
     // remove all non-audio tokens (i.e. < 151672 || > 155772)
     codes.erase(std::remove_if(codes.begin(), codes.end(), [](llama_token t) { return t < 151672 || t > 155772; }), codes.end());
 
@@ -709,12 +701,12 @@ int main(int argc, char ** argv) {
         token -= 151672;
     }
 
-    const int n_codes = codes.size();
+    batch = llama_batch_init(codes.size(), 0, 1);
 
-    batch = llama_batch_init(n_codes, 0, 1);
+    printf("codes.size(): %zu\n", codes.size());
 
     for (size_t i = 0; i < codes.size(); ++i) {
-        batch_add(batch, codes[i], i, { 0 }, true); // TODO: all logits?
+        batch_add(batch, codes[i], i, true); // TODO: all logits?
     }
 
     // evaluate the current batch with the transformer model
@@ -729,7 +721,7 @@ int main(int argc, char ** argv) {
     const int n_embd = llama_model_n_embd(vocoder);
     const float * embd = llama_get_embeddings(ctx_vocoder);
 
-    auto audio = embd_to_audio(embd, n_codes, n_embd, ctx_params.n_threads);
+    auto audio = embd_to_audio(embd, codes.size(), n_embd, ctx_params.n_threads);
 
     const std::string fname = "output.wav";
 
